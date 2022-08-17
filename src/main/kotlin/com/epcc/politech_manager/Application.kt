7e88734e97bb033650786da2be2ca28024b07d44
Roberto <92rober@gmail.com>
2022-08-17 13:49:22 +0200

    feat: added CORS in Application class

--- src/main/kotlin/com/epcc/politech_manager/Application.kt
+++ src/main/kotlin/com/epcc/politech_manager/Application.kt
@@ -2,15 +2,31 @@ package com.epcc.politech_manager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
 
 @SpringBootApplication
-class Application
+class Application {
+	@Bean
+	fun corsConfigurer(): WebMvcConfigurer? {
+		return object : WebMvcConfigurer {
+			override fun addCorsMappings(registry: CorsRegistry) {
+				registry.addMapping("/").allowedOrigins("https://politech-manager.herokuapp.com/")
+			}
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
